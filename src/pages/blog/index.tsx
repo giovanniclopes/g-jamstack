2e41ecdf6e526c1004c8a2c8d0e3cfdc9e5736fd
Giovanni Lopes <giovanniclopes@gmail.com>
2022-12-31 03:23:13 -0300

    Update index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -50,7 +50,7 @@ export default function Blog() {
 
 export const getStaticProps: GetStaticProps = async () => {
   await Promise.all([
-    await client.query(PostsDocument,).toPromise(),
+    await client.query(PostsDocument, null, null).toPromise(),
     await client.query(PageDocument, { slug: 'blog' }).toPromise(),
   ])
 
